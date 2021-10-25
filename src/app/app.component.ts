75b30b345f37142684dd14b17bc2544e4ecf83f9
Tharani <Tharani@Asplapr337.Aspiresys.com>
2021-10-25 21:17:54 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tdf';
+  gender=['Male','Female','Other'];
+  Usermodel = new User('tharani','a@any.com',9876543211,'Male','morning',true);
 }
---
+++ src/app/user.ts
@@ -0,0 +1,10 @@
+export class User {
+    constructor(
+        public name : string,
+        public email: string,
+        public number: number,
+        public gender: string,
+        public time:string,
+        public subscribe:boolean
+    ){}
+}
